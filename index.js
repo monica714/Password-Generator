3fd327abe2d12218efcfe4391a039bfc7732404b
monica714 <87255137+monica714@users.noreply.github.com>
2022-12-18 12:04:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+
+
+
+//Selectors
+
+
+
+
+const lowerCase=document.getElementById("lowercase");
+    
+const upperCase=document.getElementById("uppercase");
+
+const Numbers=document.getElementById("numbers");
+
+const Symbols=document.getElementById("symbols");
+
+const ResultBar =document.getElementById("target");
+
+const TotalLength=document.getElementById("length");
+
+
+//Random Value
+
+const UpperSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LowerSet="abcdefghijklmnopqrstuvwxyz";
+const numberSet="0123456789";
+const SymbolSet="~!@#$%^&*()_+-?/";
+
+
+const getRandomData=(dataset)=>{
+  return dataset[Math.floor(Math.random()*  dataset.length)];
+}
+
+
+const getPass=(Password="")=>{
+  
+     if(upperCase.checked)
+      {
+        Password += getRandomData(UpperSet);
+      }
+      if(lowerCase.checked)
+      {
+        Password += getRandomData(LowerSet);
+      }
+      if(Numbers.checked)
+      {
+        Password += getRandomData(numberSet);
+      }
+      if(Symbols.checked)
+      {
+        Password += getRandomData(SymbolSet);
+      }
+     if(Password.length < TotalLength.value)
+      {
+        return getPass(Password);
+      }
+      
+      ResultBar.innerHTML=truncateString(Password,TotalLength.value);
+     
+      
+}
+
+
+  
+
+
+    
+
+document.getElementById("generate").addEventListener("click",()=>{
+  
+        getPass();
+   })
+
+
+//------> Main
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num);
+  } else {
+    return str;
+  }
+}
